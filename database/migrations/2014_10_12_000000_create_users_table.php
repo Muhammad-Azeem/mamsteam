cbf970725cbcd11eab5f5c994ce2631cce5b0b58
azeemghafoor <azeemghafoor@softpers.com>
2019-12-28 19:47:30 +0500

    init commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('u_id');
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('address');
+            $table->integer('phone_number');
+            $table->integer('location');
+            $table->integer('date_of_birth');
+            $table->string('gender');
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
